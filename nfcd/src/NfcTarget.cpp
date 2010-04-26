9cca656c5b12f0112091f486317c6b13e3e62fc0
yann.peniguel <yann.peniguel@ba5c3050-9c55-11de-a262-85a698460a8e>
2010-04-26 10:18:03 +0000

    nfcd: use a switch instead of string comparing in checkAvaibleContent()
    git-svn-id: http://nfc-tools.googlecode.com/svn/trunk@266 ba5c3050-9c55-11de-a262-85a698460a8e

--- nfcd/src/NfcTarget.cpp
+++ nfcd/src/NfcTarget.cpp
@@ -93,65 +93,71 @@ NfcTarget::checkAvaibleContent()
   _accessLock->lock();
   QByteArray data;
   int resConnect = -1;
-  if ( _name == "Mifare Classic 1k") {
-    resConnect = mifare_classic_connect ( _tag );
-    if ( 0 == resConnect ) {
 
-      Mad mad = mad_read ( _tag );
-      MadAid aid;
-      aid.function_cluster_code = 0xE1;
-      aid.application_code = 0x03;
+  enum mifare_tag_type type = freefare_get_tag_type(_tag);
+  switch(type) { 
+    case CLASSIC_1K:
+    case CLASSIC_4K:
 
-      MifareClassicKey key = { 0xD3, 0xF7, 0xD3, 0xF7, 0xD3, 0xF7 };
+      resConnect = mifare_classic_connect ( _tag );
+      if ( 0 == resConnect ) {
 
-      if ( mad != NULL ) {
-        MifareSectorNumber* sectors = mifare_application_find ( mad, aid );
-        if ( sectors != NULL ) {
-          int i = 0;
-          MifareSectorNumber sector;
-          while ( ( sector = sectors[i] ) ) {
-            qDebug() << "Dump sector " << QString::number ( sector ) << "...";
-            if ( 0 == mifare_classic_authenticate ( _tag, block ( sector, 0 ), key, MFC_KEY_A ) ) {
-              for ( uint8_t b=0; b<3; b++ ) {
-                MifareClassicBlock r;
-                if ( 0 == mifare_classic_read ( _tag, block ( sector, b ), &r ) ) {
-                  data.append ( ( char* ) r, sizeof ( MifareClassicBlock ) );
-                } else {
-                  qDebug ( "Unable to read block." );
+        Mad mad = mad_read ( _tag );
+        MadAid aid;
+        aid.function_cluster_code = 0xE1;
+        aid.application_code = 0x03;
+
+        MifareClassicKey key = { 0xD3, 0xF7, 0xD3, 0xF7, 0xD3, 0xF7 };
+
+        if ( mad != NULL ) {
+          MifareSectorNumber* sectors = mifare_application_find ( mad, aid );
+          if ( sectors != NULL ) {
+            int i = 0;
+            MifareSectorNumber sector;
+            while ( ( sector = sectors[i] ) ) {
+              qDebug() << "Dump sector " << QString::number ( sector ) << "...";
+              if ( 0 == mifare_classic_authenticate ( _tag, block ( sector, 0 ), key, MFC_KEY_A ) ) {
+                for ( uint8_t b=0; b<3; b++ ) {
+                  MifareClassicBlock r;
+                  if ( 0 == mifare_classic_read ( _tag, block ( sector, b ), &r ) ) {
+                    data.append ( ( char* ) r, sizeof ( MifareClassicBlock ) );
+                  } else {
+                    qDebug ( "Unable to read block." );
+                  }
                 }
+              } else {
+                qDebug ( "Unable to authenticate on sector." );
               }
-            } else {
-              qDebug ( "Unable to authenticate on sector." );
+              i++;
             }
-            i++;
+          } else {
+            qDebug ( "No sector for aid 0x03, 0xE1" );
           }
-        } else {
-          qDebug ( "No sector for aid 0x03, 0xE1" );
-        }
         mad_free ( mad );
+        } else {
+          qDebug ( "Unable to read MAD." );
+        }  
       } else {
-        qDebug ( "Unable to read MAD." );
-      }  
-    } else {
-      qDebug() << "Unable to connect to mifare classic tag.";
-    }
-    if( mifare_classic_disconnect ( _tag ) == 0 )
-      qDebug() << "disconnected";
-    _accessLock->unlock();
-  }
-  else if (_name == "Mifare UltraLight") {
-    resConnect =  mifare_ultralight_connect (_tag);
-    if(0 == resConnect) {
-      uint8_t pageNum = 0;
-      MifareUltralightPage pages[16];
-      for(pageNum = 0; pageNum < 16; pageNum++) {
-        mifare_ultralight_read (_tag, pageNum, &(pages[pageNum]) );
-        for(uint8_t i = 0; i < 4 ; i++) data.append( pages[pageNum][i] );
+        qDebug() << "Unable to connect to mifare classic tag.";
       }
-      if( mifare_ultralight_disconnect ( _tag ) == 0 )
+      if( mifare_classic_disconnect ( _tag ) == 0 )
         qDebug() << "disconnected";
       _accessLock->unlock();
-    }
+    break;
+    case ULTRALIGHT:
+      resConnect =  mifare_ultralight_connect (_tag);
+      if(0 == resConnect) {
+        uint8_t pageNum = 0;
+        MifareUltralightPage pages[16];
+        for(pageNum = 0; pageNum < 16; pageNum++) {
+          mifare_ultralight_read (_tag, pageNum, &(pages[pageNum]) );
+          for(uint8_t i = 0; i < 4 ; i++) data.append( pages[pageNum][i] );
+        }
+        if( mifare_ultralight_disconnect ( _tag ) == 0 )
+          qDebug() << "disconnected";
+        _accessLock->unlock();
+      }
+    break;
   }
   if ( !data.isEmpty() ) {
     QString hex;
