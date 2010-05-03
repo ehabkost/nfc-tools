dacfb4d829b0fb3df452332beccc66c2fbd4d77b
yann.peniguel <yann.peniguel@ba5c3050-9c55-11de-a262-85a698460a8e>
2010-05-03 07:45:38 +0000

    desknfc: updating translation
    git-svn-id: http://nfc-tools.googlecode.com/svn/trunk@316 ba5c3050-9c55-11de-a262-85a698460a8e

--- desknfc/i18n/fr_FR.ts
+++ desknfc/i18n/fr_FR.ts
@@ -4,43 +4,37 @@
 <context>
     <name>ContentWidget</name>
     <message>
-        <location filename="build/ui_ContentWidget.h" line="112"/>
         <source>Informations</source>
-        <translation>Informations</translation>
+        <translation type="obsolete">Informations</translation>
     </message>
     <message>
-        <location filename="build/ui_ContentWidget.h" line="115"/>
         <source>Open</source>
-        <translation>Ouvrir</translation>
+        <translation type="obsolete">Ouvrir</translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="build/ui_MainWindow.h" line="110"/>
         <source>DeskNFC</source>
-        <translation>DeskNFC</translation>
+        <translation type="obsolete">DeskNFC</translation>
     </message>
     <message>
-        <location filename="build/ui_MainWindow.h" line="111"/>
         <source>Quit</source>
-        <translation>Quitter</translation>
+        <translation type="obsolete">Quitter</translation>
     </message>
     <message>
-        <location filename="build/ui_MainWindow.h" line="113"/>
         <source>NFC Devices</source>
-        <translation>Périphérique NFC</translation>
+        <translation type="obsolete">Périphérique NFC</translation>
     </message>
     <message>
-        <location filename="build/ui_MainWindow.h" line="114"/>
         <source>Full Tree</source>
-        <translation>Arborescence complète</translation>
+        <translation type="obsolete">Arborescence complète</translation>
     </message>
 </context>
 <context>
     <name>NfcDeviceManager</name>
     <message>
-        <location filename="NfcDeviceManager.cpp" line="7"/>
+        <location filename="../NfcDeviceManager.cpp" line="7"/>
         <source>please launch nfcd</source>
         <translation>veuillez lancer nfcd</translation>
     </message>
@@ -48,57 +42,63 @@
 <context>
     <name>NfcUiManager</name>
     <message>
-        <location filename="NfcUiManager.cpp" line="61"/>
+        <location filename="../NfcUiManager.cpp" line="57"/>
         <source>Device Manager</source>
         <translation>Gestionnaire de périphériques</translation>
     </message>
     <message>
-        <location filename="NfcUiManager.cpp" line="62"/>
+        <location filename="../NfcUiManager.cpp" line="58"/>
         <source>Quit</source>
         <translation>Quitter</translation>
     </message>
     <message>
-        <location filename="NfcUiManager.cpp" line="71"/>
+        <location filename="../NfcUiManager.cpp" line="67"/>
+        <location filename="../NfcUiManager.cpp" line="370"/>
         <source>Open</source>
         <translation>Ouvrir</translation>
     </message>
     <message>
-        <location filename="NfcUiManager.cpp" line="103"/>
+        <location filename="../NfcUiManager.cpp" line="97"/>
         <source>Type: </source>
         <translation>Type:</translation>
     </message>
     <message>
-        <location filename="NfcUiManager.cpp" line="156"/>
+        <location filename="../NfcUiManager.cpp" line="149"/>
         <source>File to write</source>
         <translation>Fichier à écrire</translation>
     </message>
     <message>
-        <location filename="NfcUiManager.cpp" line="156"/>
+        <location filename="../NfcUiManager.cpp" line="149"/>
         <source>Tous (*)</source>
         <translation>Tous (*)</translation>
     </message>
     <message>
-        <location filename="NfcUiManager.cpp" line="236"/>
         <source>content found on </source>
-        <translation>contenu trouvé sur </translation>
+        <translation type="obsolete">contenu trouvé sur </translation>
     </message>
     <message>
-        <location filename="NfcUiManager.cpp" line="318"/>
+        <location filename="../NfcUiManager.cpp" line="311"/>
         <source>NFC Reader</source>
         <translation>Lecteur NFC</translation>
     </message>
 </context>
 <context>
+    <name>Notification</name>
+    <message>
+        <location filename="../Notification.cpp" line="15"/>
+        <source>Type: </source>
+        <translation>Type:</translation>
+    </message>
+</context>
+<context>
     <name>TargetWidget</name>
     <message>
-        <location filename="build/ui_TargetWidget.h" line="75"/>
         <source>Informations</source>
-        <translation>Informations</translation>
+        <translation type="obsolete">Informations</translation>
     </message>
     <message>
-        <location filename="build/ui_TargetWidget.h" line="76"/>
         <source>NFC target</source>
-        <translation>Cible NFC</translation>
+        <translation type="obsolete">Cible NFC</translation>
     </message>
 </context>
 </TS>
